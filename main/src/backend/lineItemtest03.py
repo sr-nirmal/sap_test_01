38340700e6b3e6c4d6ba1862a95a7f662e715484
abi4203 <75134413+abi4203@users.noreply.github.com>
2023-05-02 18:14:06 +0530

    main_01

--- main/src/backend/lineItemtest03.py
+++ main/src/backend/lineItemtest03.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 # Load variables from .env file
 load_dotenv()
 
-
-openai.api_key = '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'
+openai.api_key = os.getenv('API_KEY')
 
 prompt = """ www.jurcelounge.inCASIDPAIDRolfacosta.RodjacostaCASH/ARECHSF Enterprise, B-9/11(CA)PAIDni Nadia,West Bengal 71Ph# 033258game:CASHSale: 19/06/22HSKYummericaPAID22:03PriesCashier: 1: 63PAIDgables AsheshCASHTemQLYSCASCHAATPrioAmountPAIDMineral WaterI3050.00OK PLEASE!Egg Fried Rcc2130260.009 Manchunanle11701/0.00CASH's ]PAIDShilly Chicken 161200200.00Pcs]Masala KulchaCRAZY165.00Pricken Kadhai10240.00CASHMinit Mojito80320.00PAIDSpicy MexicanI525525.00Large Thin CrustChicken1150140.00CASHShawarmaCASHPAIDTotal Qty:JUICE AUGG Health1961.90your first stop 16COST2.49.05SEST5.5%49.05PAIDwww.juicelounge.inCASHGrand Total2060.00CASTPAIDFSSAT ic No 128170120000GST RobbacostaThank You! AgainsAgain!AIDCAS"""
 
