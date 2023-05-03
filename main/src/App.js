21ce4455f5f437e077c91fd200ab9bdb3ea76f89
nirmal_sr <nirmalsr783@gmail.com>
2023-05-03 20:57:59 +0530

    main_01

--- main/src/App.js
+++ main/src/App.js
@@ -14,102 +14,15 @@ function App(){
   const [inputName , setInputname]= useState("");
   const [name, setName] = useState('');
 
-  // const handleLogin = (e) => {
-      
-  //     e.preventDefault();
-  
-  //     fetch('/login', {
-  //         method: 'POST',
-  //         headers: {
-  //         'Content-Type': 'application/json'
-  //         },
-  //         body: JSON.stringify({ username, password })
-  //     })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //         console.log(data.result);
-  //         if(data.result === "success"){
-  //           setcurrentState(1);
-  //         }
-  //         console.log(currentState);
-  //         // Handle the response from the server
-  //     })
-  //     .catch(error => {
-  //         console.error('Error:', error);
-  //     });
-  // }
-
-  // function LoginFrame(){
-  //     return (
-  //         <div>
-  //           <h1>Login Page</h1>
-  //           <form onSubmit={handleLogin}>
-  //             <label>
-  //               Username:
-  //               <input
-  //                 type="text"
-  //                 value={username}
-  //                 onChange={(e) => setUsername(e.target.value)}
-  //               />
-  //             </label>
-  //             <br />
-  //             <label>
-  //               Password:
-  //               <input
-  //                 type="password"
-  //                 value={password}
-  //                 onChange={(e) => setPassword(e.target.value)}
-  //               />
-  //             </label>
-  //             <br />
-  //             <button type="submit">Login</button>
-  //           </form>
-  //         </div>
-  //       );
-  //     }
       function DashBoard(props){
           return (
-              // <div>
-              //     <div className = 'left'>
-              //       <Top />
-              //     </div>
-                  
-              //     <div className = 'right'>
-              //       <button className='logout' onClick={setcurrentState(0)}>LOGOUT</button>
-              //       <Right ></Right>
-              //     </div>
-                  
-              // </div>
               <div>
                 <Top className ='left'></Top>
               </div>
           );
       }
       return (
-          // <div className='main'>
-          //   <Top className ="left"/> 
-          //   <Right className="right"/>
-          //   {/* <div className="left" >
-          //     <Top />
-          //   </div>
-            
-            
-          //   <div className="top" >
-          //     <Right />
-          //   </div> */}
-            
-          //  </div>
-          // <div className='main'>
-          //   <div className = 'left'>
-          //     <Top ></Top>
-          //   </div>
-            
-          //   <div className = 'right'>
-          //     <button className='logout' onClick={setcurrentState(0)}>LOGOUT</button>
-          //     <Right ></Right>
-          //   </div>
-                  
-          // </div>
+          
           <div className='main'>
             
             <div className='center'>
@@ -128,8 +41,6 @@ function App(){
 
             <div className='right'>
               <h1>hello name_01</h1>
-    
-
               <button onClick={() => setcurrentState(1)} className='upload_btn'> Upload </button>
               <button onClick={() => setcurrentState(0)} className='back_btn'> Home </button>
               <button onClick={() =>setcurrentState(2)} className='to_bill'> Recipts </button>
@@ -152,6 +63,8 @@ function App(){
                 )}
             </div>
           </div>
+
+          
           );
       
       function handleChange(event) {
@@ -166,25 +79,22 @@ function App(){
       }
       
       return (
-          <div>
-            {name === '' && (
-              <form onSubmit ={handleSubmit}  >
-              <label>
-                Name:
-                <input type="text" value={inputName} onChange={handleChange} />
-              </label>
-              <button type ='submit'>Submit</button>
-              </form>
-            )}
-            {name !== '' && (
-              <DashBoard>{name}</DashBoard>
+        <div>
+        {name === '' && (
+          <form onSubmit ={handleSubmit}  >
+          <label>
+            Name:
+            <input type="text" value={inputName} onChange={handleChange} />
+          </label>
+          <button type ='submit'>Submit</button>
+          </form>
+        )}
+        {name !== '' && (
+          <DashBoard>{name}</DashBoard>
 
-            )}
-          </div>
-          // <div>
-          //   <GenerateIcon>Name</GenerateIcon>
-          // </div>
-          
+        )}
+      </div>
+  
       );
 
             }
